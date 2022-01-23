2bb85aa9f71916b64bc9e04603894380a506a649
klown <es.halvers@gmail.com>
2022-01-23 17:29:43 +0900

    Create Project

---
+++ GetKmong_V2/Shared/GetKmong_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  GetKmong_V2App.swift
+//  Shared
+//
+//  Created by Klown on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GetKmong_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
